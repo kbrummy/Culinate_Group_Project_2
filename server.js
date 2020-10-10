3cf83727db4a1eafdefec5b826fe160a71f5d9a9
Brian Praseuth <brianpraseuth@gmail.com>
2020-10-10 11:23:30 -0700

    Removed code that wasn't needed.

--- server.js
+++ server.js
@@ -11,7 +11,6 @@ var express = require("express");
 var app = express();
 var PORT = process.env.PORT || 3000;
 
-
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -26,4 +25,6 @@ require("./routes/api-routes.js")(app);
 
 app.listen(PORT, function() {
     console.log("App listening on PORT " + PORT);
-});
+}); 
+
+// Updated files.
