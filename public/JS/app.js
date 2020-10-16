42e03e062699111e92823220830ef0eb29f45521
Brian Praseuth <brianpraseuth@gmail.com>
2020-10-15 17:13:24 -0700

    Updated some files

--- public/JS/app.js
+++ public/JS/app.js
@@ -1,21 +1,49 @@
 $(document).ready(() => {
+// Searches for recipes based on input
+    $("#submit").on("click", function (event) {
+        event.preventDefault();
+        var searched = document.getElementById("searchedWord").value;
+        console.log(searched);
 
-    $("#submit").on("click", function(event) {
-		event.preventDefault();
-		var searched = document.getElementById("searchedWord").value;
-		console.log(searched);
+        var entreeQueryURL = "https://www.themealdb.com/api/json/v1/1/search.php?s=" + searched;
 
-		var entreeQueryURL = "https://www.themealdb.com/api/json/v1/1/search.php?s=" + searched;
+        $.ajax({
+            url: entreeQueryURL,
+            method: "GET"
+        }).then(function (response) {
+            console.log(response);
+                var recipeColumn = $("<div>");
+                var recipeCard = $("<div>").addClass("card");
+                var content = $("<div>").addClass("card-content");
+                var entreeName = $("<h5>").text(response.meals[0].strMeal);
+                var entreeImage = $("<img>").attr("src", response.meals[0].strMealThumb).addClass("entreeImage");
+                var entreeRecipe = $("<p>").text(response.meals[0].strInstructions);
+                var saveButton = $("<button>").text("Save Recipe").addClass("saveButton");
+    
+                content.append(entreeName, entreeImage, entreeRecipe, saveButton);
+                recipeCard.append(content);
+                recipeColumn.append(recipeCard);
+                $("#recipeCards").append(recipeColumn);
 
-		$.ajax({
-			url: entreeQueryURL,
-			method: "GET"
-		}).then(function(response) {
-			console.log(response);
-			})
+                $.post("/feed", recipeCard)
+                }).then(function (response) {
+                    console.log(response);
+                        var recipeColumn = $("<div>");
+                        var recipeCard = $("<div>").addClass("card");
+                        var content = $("<div>").addClass("card-content");
+                        var entreeName = $("<h5>").text(response.meals[0].strMeal);
+                        var entreeImage = $("<img>").attr("src", response.meals[0].strMealThumb).addClass("entreeImage");
+                        var entreeRecipe = $("<p>").text(response.meals[0].strInstructions);
+                        var saveButton = $("<button>").text("Save Recipe").addClass("saveButton");
+            
+                        content.append(entreeName, entreeImage, entreeRecipe, saveButton);
+                        recipeCard.append(content);
+                        recipeColumn.append(recipeCard);
+                        $("#recipeCards").append(recipeColumn);
 
-		})
+    }) 
+})});
 
-
-});
-
+$("#saveButton").on("click", function(event) {
+    event.preventDefault();
+})
